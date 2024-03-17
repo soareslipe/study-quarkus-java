8af518a0a1ddd57290127911e21b1928e4c797f6
soareslipe <zuraffa2015@gmail.com>
2024-03-17 18:14:41 -0300

    Pesistence added in project. Greeting entity created. Method to add and read Greeting names has been created in GreetingResource.

---
+++ getting-started-dev-services/src/main/java/org/acme/Greeting.java
@@ -0,0 +1,11 @@
+package org.acme;
+
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Greeting extends PanacheEntity {
+
+    public String name;
+}
