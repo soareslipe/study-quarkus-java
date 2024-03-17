01d5c19ab5d833618b1ea924a1eaa951b254bb82
soareslipe <zuraffa2015@gmail.com>
2024-03-17 00:55:18 -0300

    Tests ok

--- getting-started/src/test/java/org/acme/GreetingResourceTest.java
+++ getting-started/src/test/java/org/acme/GreetingResourceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import java.util.UUID;
+
 @QuarkusTest
 class GreetingResourceTest {
     @Test
@@ -15,6 +17,17 @@ class GreetingResourceTest {
           .then()
              .statusCode(200)
              .body(is("Hello from RESTEasy Reactive"));
+    }
+
+    @Test
+    void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+        .pathParam("name", uuid)
+        .when().get("/hello/greeting/{name}")
+        .then()
+        .statusCode(200)
+        .body(is("Hello " + uuid));
     }
 
 }
