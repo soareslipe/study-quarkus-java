4a71e07ca6bb97987a25a7b9e24a05479f2326fb
soareslipe <zuraffa2015@gmail.com>
2024-03-17 19:10:52 -0300

    External database added. End of second app guide

---
+++ getting-started-dev-services/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO Greeting(id, name) values (nextval('Greeting_SEQ'), 'Alice');
+INSERT INTO Greeting(id, name) values (nextval('Greeting_SEQ'), 'Bob');
+INSERT INTO Greeting(id, name) values (nextval('Greeting_SEQ'), 'Carlos');
