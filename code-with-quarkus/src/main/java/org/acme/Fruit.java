c696f36ad7c9bf7e619417b07bb04f7d23376f16
Your Name <you@example.com>
2024-03-27 22:08:53 -0300

    Fruit entity created. Database definition and seed.

---
+++ code-with-quarkus/src/main/java/org/acme/Fruit.java
@@ -0,0 +1,14 @@
+package org.acme;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import jakarta.persistence.Cacheable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+@Cacheable
+public class Fruit extends PanacheEntity {
+
+    @Column(length = 40, unique = true)
+    public String name;
+}
